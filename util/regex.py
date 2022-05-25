6dfa57c3ad2716960dd9be1188d1d999031c103f
KekOnTheWorld <kekontheworld@gmail.com>
2022-05-25 21:25:49 +0200

    Removed util package

--- util/regex.py
+++
@@ -1,9 +0,0 @@
-# Regex utility functions
-def multi_replace(value, regex, valuator):
-    txt = ""
-    endidx = 0
-    res = regex.finditer(value)
-    for exp in res:
-        txt += value[endidx : exp.start(0)] + valuator(exp)
-        endidx = exp.end(0)
-    return txt + value[endidx:]
